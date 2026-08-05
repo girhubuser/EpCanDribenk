d67ff7f8b7a3a4d1d8dd0e299f2de8b1617c7400
dazhuzhu <631628843@qq.com>
2026-08-05 11:48:30 +0800

    1

--- Sources/EpCanDribenk/EpCanDribenk.swift
+++ Sources/EpCanDribenk/EpCanDribenk.swift
@@ -1,2 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+public class EpCanDribenk {
+    
+    public init() {}
+    
+    public func abj() -> Void {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
