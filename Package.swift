b4d37c7eda86405e28d89c66f71250112eb352a9
dazhuzhu <631628843@qq.com>
2026-08-05 11:24:03 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EpCanDribenk",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "EpCanDribenk",
+            targets: ["EpCanDribenk"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "EpCanDribenk"
+        ),
+
+    ],
+    swiftLanguageModes: [.v6]
+)
